d1fca9ca372e4b40e33099884e4bdc576539128c
Stefan Vukanović <lisacvukhome@gmail.com>
2018-02-15 17:33:25 +0100

    Fix the code stuff.

--- init.lua
+++ init.lua
@@ -15,7 +15,9 @@ effects_api.give_effect_to_player = function(effect_n, name)
 	local effect = effects_api.registered_effects[effect_n]
 	local current_effects = minetest.deserialize(player:get_attribute("effects_api:effects")) or {}
 	
-	current_effects[effect.name] = effects_api.registered_effects[effect.name]
+	current_effects[effect.name] = {}
+
+	effects_api.registered_effects[effect.name].on_add(name) -- Handle the on_add function.
 
 	player:set_attribute("effects_api:effects", minetest.serialize(current_effects))
 	print(minetest.serialize(current_effects))
@@ -24,20 +26,28 @@ end
 effects_api.register_effect({
 	name = "test",
 	on_loop = function(name)
-		minetest.chat_send_all("Test!")
-	end
+		--minetest.chat_send_all("Test!")
+	end,
+	on_add = function(name)
+		minetest.set_player_privs(name, {fly=true})
+		minetest.chat_send_all("Player " .. name .. " can fly like a bird!")
+	end,
+	on_remove = function(name)
+		minetest.set_player_privs(name, {fly=false})
+		minetest.chat_send_all("Player " .. name .. " can fly like a stone!")
+	end,
 })
+
 minetest.register_globalstep(function(dtime)
 	for _,player in ipairs(minetest.get_connected_players()) do
 		local name = player:get_player_name()
 		local current_effects = minetest.deserialize(player:get_attribute("effects_api:effects"))
-		--minetest.chat_send_all(dump(current_effects))
-		for _,effect in pairs(current_effects) do
-			effect.on_loop(name)
-			--minetest.chat_send_all(dump(current_effects))
+		for effect,data in pairs(current_effects) do
+			effects_api.registered_effects[effect].on_loop(name)
 		end
 	end
 end)
+
 minetest.register_on_joinplayer(function(player)
 	local name = player:get_player_name()
 	effects_api.give_effect_to_player("test", name)
